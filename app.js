fb782a8e17c066fc10671433df88147c3bb92ac0
MatthiasWanner <matthias.wanner@gmail.com>
2021-03-15 08:58:58 +0100

    Create Route with updateOne method to modify existing object in DB

--- app.js
+++ app.js
@@ -33,13 +33,21 @@ app.post('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
   });
 
-  // GET Routes
+  // GET Routes to modify an object
+  app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+      .catch(error => res.status(400).json({ error }));
+  });
+
+  // To get one item with the id
   app.get('/api/stuff/:id', (req, res, next) => {
     Thing.findOne({ _id: req.params.id })
       .then(thing => res.status(200).json(thing))
       .catch(error => res.status(404).json({ error }));
   });
-  
+
+  //To get all items in the route
 app.get('/api/stuff', (req, res, next) => {
   Thing.find()
   .then(things => res.status(200).json(things))
